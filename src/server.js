4f59f2b2cff9728852585a524e93eb7aa6c19e7b
Jose Miguel Cabrera <cabreraj525@gmail.com>
2021-03-17 01:54:02 -0600

    Agregamos seccion usuarios

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express();
 //routes
 import IndexRoutes from "./routes/index.routes";
 import ProductosRoutes from "./routes/productos.routes";
+import UsuariosRoutes from "./routes/user.routes";
 
 //Settings
 app.set('port', process.env.PORT || 3000)
@@ -15,5 +16,6 @@ app.use(json());
 //routes
 app.use(IndexRoutes);
 app.use('/product', ProductosRoutes);
+app.use('/user', UsuariosRoutes);
 
 export default app;
